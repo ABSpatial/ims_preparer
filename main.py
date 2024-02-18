b71231dae392bb26c4fb44ea0d92c47da4b9c24f
Leonid Kolesnichenko <xperience439@gmail.com>
2024-02-18 12:12:17 +0300

    Write a script for cron task

---
+++ main.py
@@ -0,0 +1,15 @@
+import pendulum
+
+from ims.preparer import prepare
+
+input_layer = "./preparer/central_asia_boundary.gpkg"
+layer_name = "central_asia_boundary"
+raster_date = pendulum.now("UTC").subtract(days=1).format("YYYY-MM-DD")
+output_crs = "EPSG:3857"
+type_codes = {
+    3: "Ice",
+    4: "Snow"
+}
+output_path = f"/tmp/{raster_date}.gpkg"
+
+prepare(input_layer, layer_name, raster_date, output_crs, type_codes, output_path)
